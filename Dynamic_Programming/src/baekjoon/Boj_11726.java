ab71b71383b5ff75c31a33526df80da3d6298e14
junu0516 <68586291+junu0516@users.noreply.github.com>
2020-12-06 19:27:14 +0900

    11726번 문체풀이 추가(2Xn 타일링)

---
+++ Dynamic_Programming/src/baekjoon/Boj_11726.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Boj_11726 {
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n+1];
+		
+		arr[1] = 1;
+		if(n>=2) {
+			arr[2] = 2;
+			for(int i=3;i<=n;i++) {
+				int temp = arr[i-2]+arr[i-1];
+				arr[i] = temp%10007; 
+			}
+		}
+		bw.write(arr[n]+"");
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
