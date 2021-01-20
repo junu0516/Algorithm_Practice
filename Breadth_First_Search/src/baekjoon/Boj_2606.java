0c65aadecf7632628f047dffac40578a1d97c6b0
junu0516 <68586291+junu0516@users.noreply.github.com>
2021-01-20 21:35:56 +0900

    백준 문제풀이 2606 복습(바이러스)

--- Breadth_First_Search/src/baekjoon/Boj_2606.java
+++ Breadth_First_Search/src/baekjoon/Boj_2606.java
@@ -1,60 +1,61 @@
 package baekjoon;
 
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Boj_2606 {
-
-	public static void main(String[] args) throws IOException {
+	static int n;
+	static int e;
+	
+	public static void main(String[] args){
 		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		Scanner sc = new Scanner(System.in);
 		
-		int num = Integer.parseInt(br.readLine());
-		int edge = Integer.parseInt(br.readLine());
+		n = sc.nextInt(); //노드의 개수
+		e = sc.nextInt(); //엣지의 개수
 		
-		LinkedList<Integer>[] map = new LinkedList[num+1];
-		boolean[] visited = new boolean[num+1];
-		for(int i=0;i<num+1;i++) {
+		LinkedList<Integer>[] map = new LinkedList[n+1];
+		/*
+		 * 주의 : LinkedList<>로 배열을 선언했을 때, 초기값이 null이기 때문에 반드시 배열의 처음부터 끝까지 빈 리스트를 할당해줘야 함
+		 * 그렇지 않을 경우, NullPointerException 발생
+		 * */
+		for(int i=0;i<map.length;i++) {
 			map[i] = new LinkedList<Integer>();
 		}
-		for(int i=0;i<edge;i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int y = Integer.parseInt(st.nextToken());
-			int x = Integer.parseInt(st.nextToken());
-	
-			map[y].add(x);
-			map[x].add(y);
-		}	
 		
-		int result = bfs(map,visited,1);
+		for(int i=0;i<e;i++) {
+			int start = sc.nextInt();
+			int end = sc.nextInt();
+			
+			map[start].add(end);
+			map[end].add(start);
+		}
 		
-		bw.write(result+"");
-		
-		br.close();
-		bw.flush();
-		bw.close();
+		int result = bfs(map);
+		System.out.println(result-1); //1번 노드는 제외
 	}
 	
-	public static int bfs(LinkedList<Integer>[] map, boolean[] visited, int start) {
+	public static int bfs(LinkedList<Integer>[] map) {
+		boolean[] visited = new boolean[n+1];
+		Queue<Integer> toVisit = new LinkedList<>();
+		toVisit.add(1);
+		int count = 0;
 		
-		Queue<Integer> toVisit = new LinkedList<Integer>();
-		toVisit.add(start);
-		int infected = 0;
-		visited[start] = true;
 		while(!toVisit.isEmpty()) {
 			int curr = toVisit.poll();
 			
 			for(int i : map[curr]) {
 				if(!visited[i]) {
-					toVisit.add(i);
 					visited[i] = true;
-					infected++;
+					toVisit.add(i);
+					count++;
 				}
 			}
+			
 		}
-		
-		return infected;
+		return count;
 	}
+	
 
 }
