32ae02e1d3b17dfc976ab7d3ffb179675eedec8b
junu0516 <68586291+junu0516@users.noreply.github.com>
2021-01-22 22:28:16 +0900

    백준 문제풀이 2667 복습(단지 번호 붙이기)

--- Breadth_First_Search/src/baekjoon/Boj_2667.java
+++ Breadth_First_Search/src/baekjoon/Boj_2667.java
@@ -3,64 +3,62 @@ package baekjoon;
 import java.util.*;
 
 public class Boj_2667 {
-
+	static int n;
+	static ArrayList<Integer> list = new ArrayList<>();
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		int[][] map = new int[n][n];
+		boolean[][] visited = new boolean[n][n];
 		
-		int size = sc.nextInt();
-		int[][] area = new int[size][size];
-		boolean[][] visited = new boolean[size][size];
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		
-		for(int i=0;i<size;i++) {
+		for(int i=0;i<n;i++) {
 			String input = sc.next();
-			for(int j=0;j<size;j++) {
-				area[i][j] = Integer.parseInt(input.substring(j,j+1));
+			for(int j=0;j<n;j++) {
+				map[i][j] = Integer.parseInt(input.substring(j,j+1));
 			}
 		}
 		
 		int total = 0;
-		for(int i=0;i<size;i++) {
-			for(int j=0;j<size;j++) {
-				if(area[i][j]==1&&visited[i][j]==false) {
-					int num = bfs(area,visited,i,j);
-					list.add(num);
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if(visited[i][j]==false && map[i][j]==1) {
+					bfs(map,visited,i,j);
 					total++;
 				}
 			}
 		}
 		
-		list.sort(null);
-		
 		System.out.println(total);
-		for(int i:list) {
+		list.sort(null);
+		for(int i : list) {
 			System.out.println(i);
 		}
+		
 	}
-
-	public static int bfs(int[][]area, boolean[][]visited, int y, int x) {
-		Queue<int[]> toVisit = new LinkedList<int[]>();
-		int[] dx = new int[] {-1,1,0,0};
-		int[] dy = new int[] {0,0,-1,1};
-		int[] start = new int[] {y,x};
-		toVisit.add(start);
+	
+	public static void bfs(int[][] map, boolean[][] visited, int y, int x) {
+		Queue<int[]> toVisit = new LinkedList<>();
+		toVisit.add(new int[] {y,x});
+		int[] dy = new int[] {-1,1,0,0};
+		int[] dx = new int[] {0,0,-1,1};
 		
-		int count=0;
+		int count = 0;
 		while(!toVisit.isEmpty()) {
-			int[] point = toVisit.poll();
-			int currX = point[1];
-			int currY = point[0];
-		
-			for(int i=0;i<4;i++) {
+			int[] curr = toVisit.poll();
+			int currY = curr[0];
+			int currX = curr[1];
+			
+			for(int i=0;i<4;i++){
 				int nextY = currY+dy[i];
 				int nextX = currX+dx[i];
 				
-				if(nextX<0||nextY<0||nextX>=visited.length||nextY>=visited.length) {
+				if(nextY<0 || nextX<0 || nextY>=n || nextX>=n) {
 					continue;
 				}
 				
-				if(visited[nextY][nextX]==true||area[nextY][nextX]==0) {
+				if(map[nextY][nextX]==0 || visited[nextY][nextX]) {
 					continue;
 				}
 				
@@ -68,13 +66,17 @@ public class Boj_2667 {
 				visited[nextY][nextX] = true;
 				count++;
 			}
-		}
+ 		}
 		
+		//단지 내에 존재하는 집의 수가 1개일 경우에는, nextY, nextX가 존재하지 않기 때문에 1개를 카운트할 수 없음
+		//일단 bfs함수에 들어왔다는 것은 곧 집이 적어도 1개이상 존재한다는 뜻이기 때문에, 반복문을 빠져나왔을 때 count=0이면 count++ 해주면 됨
 		if(count==0) {
 			count++;
 		}
 		
-		return count;
+		list.add(count);
 	}
+
+	
 	
 }
