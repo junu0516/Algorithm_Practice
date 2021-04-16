6f962cce41376045b8a5c1080be09bc21a7392b9
junu0516 <68586291+junu0516@users.noreply.github.com>
2021-04-17 08:43:21 +0900

    백준 문제풀이 1931 추가(회의실 배정/그리디알고리즘)

---
+++ GreedyAlgorithm/src/baekjoon/Boj_1931.java
@@ -0,0 +1,68 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Boj_1931 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		ArrayList<int[]> times = new ArrayList<>();
+		
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		for(int i=0;i<n;i++) {
+			st = new StringTokenizer(br.readLine());
+			int[] time = new int[2];
+			time[0] = Integer.parseInt(st.nextToken());
+			time[1] = Integer.parseInt(st.nextToken());
+			times.add(time);
+		}
+		
+		//종료시간 기준으로 회의 배열을 정렬
+		times.sort(new Comparator<int[]>() {
+
+			@Override
+			public int compare(int[] t1, int[] t2) {
+				
+				//종료시간이 같을 경우에는 시작시간 기준으로 정렬
+				if(t1[1] == t2[1]) {
+					return t1[0] - t2[0];
+				}
+				
+				return t1[1]-t2[1];
+			}
+		});
+		
+		int count = 0; //회의의 개수
+		int prev = 0; //이전 회의 종료시간
+		
+		/*
+		 * prev = 0인 이유,
+		 * 
+		 * 이전회의의 종료시간과 겹치지 않는 선에서 최대한 많은 활동을 선택해야 하는 경우이기 때문에		 * 
+		 * 종료시간 기준으로 오름차순 정렬되어 있을 때
+		 * 제일 먼저 오는 활동부터 최대한 선택되어야 최대의 개수를 도출할 수 있기 때문
+		 * 
+		 * */
+		
+		//이전 회의의 종료시간이 다음 회의의 시작시간 이하인 경우에 count+1
+		for(int i=0;i<times.size();i++) {
+			if(prev<=times.get(i)[0]) {
+				prev = times.get(i)[1]; //이전 회의 종료시간을 다시 갱신해줘야 함
+				count++;
+			}
+		}
+		
+		bw.write(count+"");
+		
+		br.close();
+		bw.flush();
+		bw.close();
+
+	}
+
+}
